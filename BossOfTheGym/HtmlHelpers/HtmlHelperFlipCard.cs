30ebbadb79c6698e09300cd917804c6e5a4e9d1a
Mouse Mickey <mickeymouse.kz.ru@outlook.com>
2022-08-23 08:10:23 +0300

    Create base code for HtmlHelper, for Flip Cards, and modify code JSCode for animation

---
+++ BossOfTheGym/HtmlHelpers/HtmlHelperFlipCard.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BossOfTheGym.HtmlHelpers
+{
+    public static class HtmlHelperFlipCard
+    {
+        public static HtmlString CreateFlipCard(this IHtmlHelper html)
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append("<div class='col-4 d-flex justify-content-center'>");
+            result.Append("<div class='coacher-card__wrapper'>");
+            result.Append("<div class='coacher-card__top'>");
+            result.Append("<div class='coacher-img' style=background:url('" + "''" + "') 50% 50% / cover no-repeat></div>");
+            result.Append("</div>");
+            result.Append("<div class='coacher-card__back d-flex flex-column justify-content-center align-items-center'>");
+            result.Append("<div class='underline-text__wrapper'>");
+            result.Append("<h5 class='unline-text'>Something</h5><div class='unline bulgarianRose'></div>");
+            result.Append("</div><p>Something</p>");
+            result.Append("</div></div></div>");
+
+            HtmlString htmlResult = new HtmlString(result.ToString());
+            return htmlResult;
+        }
+    }
+}
--- BossOfTheGym/wwwroot/js/ScriptForServices.js
+++ BossOfTheGym/wwwroot/js/ScriptForServices.js
@@ -1,11 +1,11 @@
 ﻿//Create flip card
 document.querySelectorAll(".coacher-card__wrapper").forEach(elem => {
     elem.addEventListener("mousemove", function (event) {
-        this.querySelector(".coacher-card__top").style.transform = "rotateY(90deg)";
-        this.querySelector(".coacher-card__back").style.transform = "rotateY(0deg)";
+        this.querySelector(".coacher-card__top").style.transform = "rotateX(90deg) rotateY(90deg)";
+        this.querySelector(".coacher-card__back").style.transform = "rotateX(0deg) rotateY(0deg)";
     });
     elem.addEventListener("mouseout", function (event) {
-        this.querySelector(".coacher-card__top").style.transform = "rotateY(0deg)";
-        this.querySelector(".coacher-card__back").style.transform = "rotateY(-90deg)";
+        this.querySelector(".coacher-card__top").style.transform = "rotateX(0deg) rotateY(0deg)";
+        this.querySelector(".coacher-card__back").style.transform = "rotateX(-90deg) rotateY(-90deg)";
     });
 });
