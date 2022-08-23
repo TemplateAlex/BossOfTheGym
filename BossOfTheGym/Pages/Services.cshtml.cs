e64dc7a4a18bb1f4e2e58e302f7eb4cee213e60c
Mouse Mickey <mickeymouse.kz.ru@outlook.com>
2022-08-23 08:47:38 +0300

    New algorithm, for cards and their location

--- BossOfTheGym/Pages/Services.cshtml.cs
+++ BossOfTheGym/Pages/Services.cshtml.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BossOfTheGym.Models;
 
 namespace BossOfTheGym.Pages
 {
     public class ServicesModel : PageModel
     {
-
+        private readonly BOTGDBContext _context;
+        public List<Coacher> Coachers { get; private set; } = new List<Coacher>();
         public string? Category { get; private set; }
+
         public IActionResult OnGet(string category)
         {
             Category = category;
