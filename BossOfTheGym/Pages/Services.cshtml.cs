f614ca593a5302c5da117c5dfff3d8d6e0a17ab4
Mouse Mickey <mickeymouse.kz.ru@outlook.com>
2022-08-24 09:04:57 +0300

    New algorithm for page "Services" (this is demo version of algorithm, and you can throw exception)

--- BossOfTheGym/Pages/Services.cshtml.cs
+++ BossOfTheGym/Pages/Services.cshtml.cs
@@ -7,6 +7,8 @@ namespace BossOfTheGym.Pages
     public class ServicesModel : PageModel
     {
         private readonly BOTGDBContext _context;
+
+        public TypeCoacher? typeCoacher; 
         public List<Coacher> Coachers { get; private set; } = new List<Coacher>();
         public string? Category { get; private set; }
 
@@ -16,7 +18,34 @@ namespace BossOfTheGym.Pages
 
             if(Category != "Main" && Category != "Martial_Arts" && Category != "Online_Training")
             {
+                this.typeCoacher = null;
                 return RedirectToPage("Index");
+            }
+
+
+            if (Category == "Main")
+            {
+                this.typeCoacher = _context.TypeCoachers.FirstOrDefault(t => t.TypeCoacherName == "Base");
+            }
+
+            if (Category == "Martial_Arts")
+            {
+                this.typeCoacher = _context.TypeCoachers.FirstOrDefault(t => t.TypeCoacherName == "Martial Arts");
+            }
+
+            if (Category == "Online_Training")
+            {
+                this.typeCoacher = _context.TypeCoachers.FirstOrDefault(t => t.TypeCoacherName == "Online Training");
+            }
+
+            var personsGym = _context.PersonsGym.ToList();
+
+            for (int i = 0; i < personsGym.Count; i++)
+            {
+                if (this.typeCoacher != null && this.typeCoacher.Id == personsGym[i].TypeCoacherId)
+                {
+                    Coachers.Add(_context.Coachers.FirstOrDefault(c => c.Id == personsGym[i].CoacherId));
+                }
             }
 
             return Page();
